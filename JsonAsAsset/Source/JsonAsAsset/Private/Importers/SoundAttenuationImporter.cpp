d4a56a1348695b5b50ca6336c7c14afb048ad769
Niklas <liiked1412@gmail.com>
2023-04-25 19:45:53 +0200

    Remove Derived Assets and move into their respective Importers and finish cleaning up SoundAttenuationImporter

--- JsonAsAsset/Source/JsonAsAsset/Private/Importers/SoundAttenuationImporter.cpp
+++ JsonAsAsset/Source/JsonAsAsset/Private/Importers/SoundAttenuationImporter.cpp
@@ -36,183 +36,98 @@ bool USoundAttenuationImporter::ImportData() {
 			SoundAttenuation->Attenuation.bSpatialize = Attenuation->GetBoolField("bSpatialize");
 		if (bool bUseComplexCollisionForOcclusion; Attenuation->TryGetBoolField("bUseComplexCollisionForOcclusion", bUseComplexCollisionForOcclusion))
 			SoundAttenuation->Attenuation.bUseComplexCollisionForOcclusion = Attenuation->GetBoolField("bUseComplexCollisionForOcclusion");
-
-		int64 BinauralRadius;
-		int64 FocusAttackInterpSpeed;
-		int64 FocusAzimuth;
-		int64 FocusDistanceScale;
-		int64 FocusPriorityScale;
-		int64 FocusReleaseInterpSpeed;
-		int64 FocusVolumeAttenuation;
-
-		int64 HPFFrequencyAtMax;
-		int64 HPFFrequencyAtMin;
-		int64 LPFRadiusMax;
-		int64 LPFRadiusMin;
-
-		int64 ManualPriorityAttenuation;
-		int64 ManualReverbSendLevel;
-		int64 NonFocusAzimuth;
-		int64 NonFocusDistanceScale;
-		int64 NonFocusPriorityScale;
-		int64 NonFocusVolumeAttenuation;
-		int64 OcclusionInterpolationTime;
-		int64 OcclusionLowPassFilterFrequency;
-		int64 OcclusionVolumeAttenuation;
-		int64 PriorityAttenuationDistanceMax;
-		int64 PriorityAttenuationDistanceMin;
-		int64 PriorityAttenuationMax;
-		int64 PriorityAttenuationMin;
-		int64 ReverbDistanceMax;
-		int64 ReverbDistanceMin;
-		int64 ReverbWetLevelMax;
-		int64 ReverbWetLevelMin;
-		int64 StereoSpread;
-		int64 ConeOffset;
-		int64 dBAttenuationAtMax;
-		int64 FalloffDistance;
-
-		if (Attenuation->TryGetNumberField("ConeOffset", ConeOffset)) {
+		
+		//ints
+		if (int64 ConeOffset; Attenuation->TryGetNumberField("ConeOffset", ConeOffset))
 			SoundAttenuation->Attenuation.ConeOffset = Attenuation->GetNumberField("ConeOffset");
-		}
-		if (Attenuation->TryGetNumberField("dBAttenuationAtMax", dBAttenuationAtMax)) {
+		if (int64 dBAttenuationAtMax; Attenuation->TryGetNumberField("dBAttenuationAtMax", dBAttenuationAtMax))
 			SoundAttenuation->Attenuation.dBAttenuationAtMax = Attenuation->GetNumberField("dBAttenuationAtMax");
-		}
-		if (Attenuation->TryGetNumberField("FalloffDistance", FalloffDistance)) {
+		if (int64 FalloffDistance; Attenuation->TryGetNumberField("FalloffDistance", FalloffDistance))
 			SoundAttenuation->Attenuation.FalloffDistance = Attenuation->GetNumberField("FalloffDistance");
-		}
-		if (Attenuation->TryGetNumberField("BinauralRadius", BinauralRadius)) {
+		if (int64 BinauralRadius; Attenuation->TryGetNumberField("BinauralRadius", BinauralRadius))
 			SoundAttenuation->Attenuation.BinauralRadius = Attenuation->GetNumberField("BinauralRadius");
-		}
-		if (Attenuation->TryGetNumberField("FocusAttackInterpSpeed", FocusAttackInterpSpeed)) {
+		if (int64 FocusAttackInterpSpeed;Attenuation->TryGetNumberField("FocusAttackInterpSpeed", FocusAttackInterpSpeed)) 
 			SoundAttenuation->Attenuation.FocusAttackInterpSpeed = Attenuation->GetNumberField("FocusAttackInterpSpeed");
-		}
-		if (Attenuation->TryGetNumberField("FocusAzimuth", FocusAzimuth)) {
+		if (int64 FocusAzimuth; Attenuation->TryGetNumberField("FocusAzimuth", FocusAzimuth))
 			SoundAttenuation->Attenuation.FocusAzimuth = Attenuation->GetNumberField("FocusAzimuth");
-		}
-		if (Attenuation->TryGetNumberField("FocusDistanceScale", FocusDistanceScale)) {
+		if (int64 FocusDistanceScale; Attenuation->TryGetNumberField("FocusDistanceScale", FocusDistanceScale))
 			SoundAttenuation->Attenuation.FocusDistanceScale = Attenuation->GetNumberField("FocusDistanceScale");
-		}
-		if (Attenuation->TryGetNumberField("FocusPriorityScale", FocusPriorityScale)) {
+		if (int64 FocusPriorityScale; Attenuation->TryGetNumberField("FocusPriorityScale", FocusPriorityScale)) 
 			SoundAttenuation->Attenuation.FocusPriorityScale = Attenuation->GetNumberField("FocusPriorityScale");
-		}
-		if (Attenuation->TryGetNumberField("FocusReleaseInterpSpeed", FocusReleaseInterpSpeed)) {
+		if (int64 FocusReleaseInterpSpeed; Attenuation->TryGetNumberField("FocusReleaseInterpSpeed", FocusReleaseInterpSpeed)) 
 			SoundAttenuation->Attenuation.FocusReleaseInterpSpeed = Attenuation->GetNumberField("FocusReleaseInterpSpeed");
-		}
-		if (Attenuation->TryGetNumberField("FocusVolumeAttenuation", FocusVolumeAttenuation)) {
+		if (int64 FocusVolumeAttenuation; Attenuation->TryGetNumberField("FocusVolumeAttenuation", FocusVolumeAttenuation)) 
 			SoundAttenuation->Attenuation.FocusVolumeAttenuation = Attenuation->GetNumberField("FocusVolumeAttenuation");
-		}
-		if (Attenuation->TryGetNumberField("HPFFrequencyAtMax", HPFFrequencyAtMax)) {
+		if (int64 HPFFrequencyAtMax; Attenuation->TryGetNumberField("HPFFrequencyAtMax", HPFFrequencyAtMax)) 
 			SoundAttenuation->Attenuation.HPFFrequencyAtMax = Attenuation->GetNumberField("HPFFrequencyAtMax");
-		}
-		if (Attenuation->TryGetNumberField("HPFFrequencyAtMin", HPFFrequencyAtMin)) {
+		if (int64 HPFFrequencyAtMin; Attenuation->TryGetNumberField("HPFFrequencyAtMin", HPFFrequencyAtMin)) 
 			SoundAttenuation->Attenuation.HPFFrequencyAtMin = Attenuation->GetNumberField("HPFFrequencyAtMin");
-		}
-		if (Attenuation->TryGetNumberField("ManualPriorityAttenuation", ManualPriorityAttenuation)) {
+		if (int64 ManualPriorityAttenuation; Attenuation->TryGetNumberField("ManualPriorityAttenuation", ManualPriorityAttenuation)) 
 			SoundAttenuation->Attenuation.ManualPriorityAttenuation = Attenuation->GetNumberField("ManualPriorityAttenuation");
-		}
-		if (Attenuation->TryGetNumberField("LPFRadiusMin", LPFRadiusMin)) {
+		if (int64 LPFRadiusMin; Attenuation->TryGetNumberField("LPFRadiusMin", LPFRadiusMin)) 
 			SoundAttenuation->Attenuation.LPFRadiusMin = Attenuation->GetNumberField("LPFRadiusMin");
-		}
-		if (Attenuation->TryGetNumberField("LPFRadiusMax", LPFRadiusMax)) {
+		if (int64 LPFRadiusMax; Attenuation->TryGetNumberField("LPFRadiusMax", LPFRadiusMax)) 
 			SoundAttenuation->Attenuation.LPFRadiusMax = Attenuation->GetNumberField("LPFRadiusMax");
-		}
-		if (Attenuation->TryGetNumberField("ManualReverbSendLevel", ManualReverbSendLevel)) {
+		if (int64 ManualReverbSendLevel; Attenuation->TryGetNumberField("ManualReverbSendLevel", ManualReverbSendLevel)) 
 			SoundAttenuation->Attenuation.ManualReverbSendLevel = Attenuation->GetNumberField("ManualReverbSendLevel");
-		}
-		if (Attenuation->TryGetNumberField("NonFocusAzimuth", NonFocusAzimuth)) {
+		if (int64 NonFocusAzimuth; Attenuation->TryGetNumberField("NonFocusAzimuth", NonFocusAzimuth)) 
 			SoundAttenuation->Attenuation.NonFocusAzimuth = Attenuation->GetNumberField("NonFocusAzimuth");
-		}
-		if (Attenuation->TryGetNumberField("NonFocusDistanceScale", NonFocusDistanceScale)) {
+		if (int64 NonFocusDistanceScale; Attenuation->TryGetNumberField("NonFocusDistanceScale", NonFocusDistanceScale)) 
 			SoundAttenuation->Attenuation.NonFocusDistanceScale = Attenuation->GetNumberField("NonFocusDistanceScale");
-		}
-		if (Attenuation->TryGetNumberField("NonFocusPriorityScale", NonFocusPriorityScale)) {
+		if (int64 NonFocusPriorityScale; Attenuation->TryGetNumberField("NonFocusPriorityScale", NonFocusPriorityScale)) 
 			SoundAttenuation->Attenuation.NonFocusPriorityScale = Attenuation->GetNumberField("NonFocusPriorityScale");
-		}
-		if (Attenuation->TryGetNumberField("NonFocusVolumeAttenuation", NonFocusVolumeAttenuation)) {
+		if (int64 NonFocusVolumeAttenuation; Attenuation->TryGetNumberField("NonFocusVolumeAttenuation", NonFocusVolumeAttenuation)) 
 			SoundAttenuation->Attenuation.NonFocusVolumeAttenuation = Attenuation->GetNumberField("NonFocusVolumeAttenuation");
-		}
-		if (Attenuation->TryGetNumberField("OcclusionInterpolationTime", OcclusionInterpolationTime)) {
+		if (int64 OcclusionInterpolationTime; Attenuation->TryGetNumberField("OcclusionInterpolationTime", OcclusionInterpolationTime)) 
 			SoundAttenuation->Attenuation.OcclusionInterpolationTime = Attenuation->GetNumberField("OcclusionInterpolationTime");
-		}
-		if (Attenuation->TryGetNumberField("OcclusionLowPassFilterFrequency", OcclusionLowPassFilterFrequency)) {
+		if (int64 OcclusionLowPassFilterFrequency; Attenuation->TryGetNumberField("OcclusionLowPassFilterFrequency", OcclusionLowPassFilterFrequency)) 
 			SoundAttenuation->Attenuation.OcclusionLowPassFilterFrequency = Attenuation->GetNumberField("OcclusionLowPassFilterFrequency");
-		}
-		if (Attenuation->TryGetNumberField("OcclusionVolumeAttenuation", OcclusionVolumeAttenuation)) {
+		if (int64 OcclusionVolumeAttenuation; Attenuation->TryGetNumberField("OcclusionVolumeAttenuation", OcclusionVolumeAttenuation)) 
 			SoundAttenuation->Attenuation.OcclusionVolumeAttenuation = Attenuation->GetNumberField("OcclusionVolumeAttenuation");
-		}
 #if (ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 3) || ENGINE_MAJOR_VERSION == 4
-		int64 OmniRadius;
 
-		if (Attenuation->TryGetNumberField("OmniRadius", OmniRadius)) {
+		if (int64 OmniRadius; Attenuation->TryGetNumberField("OmniRadius", OmniRadius)) 
 			SoundAttenuation->Attenuation.OmniRadius = Attenuation->GetNumberField("OmniRadius");
-		}
 #endif
-		if (Attenuation->TryGetNumberField("PriorityAttenuationDistanceMax", PriorityAttenuationDistanceMax)) {
+		if (int64 PriorityAttenuationDistanceMax; Attenuation->TryGetNumberField("PriorityAttenuationDistanceMax", PriorityAttenuationDistanceMax)) 
 			SoundAttenuation->Attenuation.PriorityAttenuationDistanceMax = Attenuation->GetNumberField("PriorityAttenuationDistanceMax");
-		}
-		if (Attenuation->TryGetNumberField("PriorityAttenuationDistanceMin", PriorityAttenuationDistanceMin)) {
+		if (int64 PriorityAttenuationDistanceMin; Attenuation->TryGetNumberField("PriorityAttenuationDistanceMin", PriorityAttenuationDistanceMin)) 
 			SoundAttenuation->Attenuation.PriorityAttenuationDistanceMin = Attenuation->GetNumberField("PriorityAttenuationDistanceMin");
-		}
-		if (Attenuation->TryGetNumberField("PriorityAttenuationMax", PriorityAttenuationMax)) {
+		if (int64 PriorityAttenuationMax; Attenuation->TryGetNumberField("PriorityAttenuationMax", PriorityAttenuationMax)) 
 			SoundAttenuation->Attenuation.PriorityAttenuationMax = Attenuation->GetNumberField("PriorityAttenuationMax");
-		}
-		if (Attenuation->TryGetNumberField("PriorityAttenuationMin", PriorityAttenuationMin)) {
+		if (int64 PriorityAttenuationMin; Attenuation->TryGetNumberField("PriorityAttenuationMin", PriorityAttenuationMin)) 
 			SoundAttenuation->Attenuation.PriorityAttenuationMin = Attenuation->GetNumberField("PriorityAttenuationMin");
-		}
-		if (Attenuation->TryGetNumberField("ReverbDistanceMax", ReverbDistanceMax)) {
+		if (int64 ReverbDistanceMax; Attenuation->TryGetNumberField("ReverbDistanceMax", ReverbDistanceMax)) 
 			SoundAttenuation->Attenuation.ReverbDistanceMax = Attenuation->GetNumberField("ReverbDistanceMax");
-		}
-		if (Attenuation->TryGetNumberField("ReverbDistanceMin", ReverbDistanceMin)) {
+		if (int64 ReverbDistanceMin; Attenuation->TryGetNumberField("ReverbDistanceMin", ReverbDistanceMin)) 
 			SoundAttenuation->Attenuation.ReverbDistanceMin = Attenuation->GetNumberField("ReverbDistanceMin");
-		}
-		if (Attenuation->TryGetNumberField("ReverbWetLevelMax", ReverbWetLevelMax)) {
+		if (int64 ReverbWetLevelMax; Attenuation->TryGetNumberField("ReverbWetLevelMax", ReverbWetLevelMax)) 
 			SoundAttenuation->Attenuation.ReverbWetLevelMax = Attenuation->GetNumberField("ReverbWetLevelMax");
-		}
-		if (Attenuation->TryGetNumberField("ReverbWetLevelMin", ReverbWetLevelMin)) {
+		if (int64 ReverbWetLevelMin; Attenuation->TryGetNumberField("ReverbWetLevelMin", ReverbWetLevelMin)) 
 			SoundAttenuation->Attenuation.ReverbWetLevelMin = Attenuation->GetNumberField("ReverbWetLevelMin");
-		}
-		if (Attenuation->TryGetNumberField("StereoSpread", StereoSpread)) {
+		if (int64 StereoSpread; Attenuation->TryGetNumberField("StereoSpread", StereoSpread)) 
 			SoundAttenuation->Attenuation.StereoSpread = Attenuation->GetNumberField("StereoSpread");
-		}
 
-		FString AbsorptionMethod;
-		FString OcclusionTraceChannel;
-		FString PriorityAttenuationMethod;
-		FString ReverbSendMethod;
-		FString SpatializationAlgorithm;
-		FString AttenuationShape;
-		FString DistanceAlgorithm;
-		FString FalloffMode;
-
-		if (Attenuation->TryGetStringField("AbsorptionMethod", AbsorptionMethod)) {
+		//Strings
+		if (FString AbsorptionMethod; Attenuation->TryGetStringField("AbsorptionMethod", AbsorptionMethod)) 
 			SoundAttenuation->Attenuation.AbsorptionMethod = static_cast<EAirAbsorptionMethod>(StaticEnum<EAirAbsorptionMethod>()->GetValueByNameString(AbsorptionMethod));
-		}
-		if (Attenuation->TryGetStringField("PriorityAttenuationMethod", PriorityAttenuationMethod)) {
+		if (FString PriorityAttenuationMethod; Attenuation->TryGetStringField("PriorityAttenuationMethod", PriorityAttenuationMethod)) 
 			SoundAttenuation->Attenuation.PriorityAttenuationMethod = static_cast<EPriorityAttenuationMethod>(StaticEnum<EPriorityAttenuationMethod>()->GetValueByNameString(PriorityAttenuationMethod));
-		}
-		if (Attenuation->TryGetStringField("ReverbSendMethod", ReverbSendMethod)) {
+		if (FString ReverbSendMethod; Attenuation->TryGetStringField("ReverbSendMethod", ReverbSendMethod)) 
 			SoundAttenuation->Attenuation.ReverbSendMethod = static_cast<EReverbSendMethod>(StaticEnum<EReverbSendMethod>()->GetValueByNameString(ReverbSendMethod));
-		}
-		if (Attenuation->TryGetStringField("FalloffMode", FalloffMode)) {
+		if (FString FalloffMode; Attenuation->TryGetStringField("FalloffMode", FalloffMode))
 			SoundAttenuation->Attenuation.FalloffMode = static_cast<ENaturalSoundFalloffMode>(StaticEnum<ENaturalSoundFalloffMode>()->GetValueByNameString(FalloffMode));
-		}
-		if (Attenuation->TryGetStringField("DistanceAlgorithm", DistanceAlgorithm)) {
-			SoundAttenuation->Attenuation.DistanceAlgorithm = static_cast<EAttenuationDistanceModel>(StaticEnum<EAttenuationDistanceModel>()->GetValueByNameString(FalloffMode));
-		}
-		if (Attenuation->TryGetStringField("SpatializationAlgorithm", SpatializationAlgorithm)) {
+		if (FString DistanceAlgorithm; Attenuation->TryGetStringField("DistanceAlgorithm", DistanceAlgorithm)) 
+			SoundAttenuation->Attenuation.DistanceAlgorithm = static_cast<EAttenuationDistanceModel>(StaticEnum<EAttenuationDistanceModel>()->GetValueByNameString(DistanceAlgorithm));
+		if (FString SpatializationAlgorithm; Attenuation->TryGetStringField("SpatializationAlgorithm", SpatializationAlgorithm)) 
 			SoundAttenuation->Attenuation.SpatializationAlgorithm = static_cast<ESoundSpatializationAlgorithm>(StaticEnum<ESoundSpatializationAlgorithm>()->GetValueByNameString(SpatializationAlgorithm));
-		}
-		if (Attenuation->TryGetStringField("AttenuationShape", AttenuationShape)) {
+		if (FString AttenuationShape; Attenuation->TryGetStringField("AttenuationShape", AttenuationShape)) 
 			SoundAttenuation->Attenuation.AttenuationShape = static_cast<EAttenuationShape::Type>(StaticEnum<EAttenuationShape::Type>()->GetValueByNameString(AttenuationShape));
-		}
-		if (Attenuation->TryGetStringField("OcclusionTraceChannel", OcclusionTraceChannel)) {
+		if (FString OcclusionTraceChannel; Attenuation->TryGetStringField("OcclusionTraceChannel", OcclusionTraceChannel)) 
 			SoundAttenuation->Attenuation.OcclusionTraceChannel = static_cast<ECollisionChannel>(StaticEnum<ECollisionChannel>()->GetValueByNameString(OcclusionTraceChannel));
-		}
 
-		if (const TSharedPtr<FJsonObject>* AttenuationShapeExtents; Attenuation->TryGetObjectField("AttenuationShapeExtents", AttenuationShapeExtents)) {
+		if (const TSharedPtr<FJsonObject>* AttenuationShapeExtents; Attenuation->TryGetObjectField("AttenuationShapeExtents", AttenuationShapeExtents)) 
 			SoundAttenuation->Attenuation.AttenuationShapeExtents = FMathUtilities::ObjectToVector(AttenuationShapeExtents->Get());
-		}
 
 		if (const TSharedPtr<FJsonObject>* CustomAttenuationCurve; Attenuation->TryGetObjectField("CustomAttenuationCurve", CustomAttenuationCurve)) {
 			TArray<TSharedPtr<FJsonValue>> Keys = Attenuation->GetObjectField("CustomAttenuationCurve")->GetObjectField("EditorCurveData")->GetArrayField("Keys");
--- JsonAsAsset/Source/JsonAsAsset/Public/Importers/SkeletonImporter.h
+++ JsonAsAsset/Source/JsonAsAsset/Public/Importers/SkeletonImporter.h
@@ -4,6 +4,13 @@
 
 #include "Importer.h"
 
+// We use this to set variables
+// in the skeletal asset
+class USkeletonAssetDerived : public USkeleton {
+public:
+	bool AddVirtualBone(const FName SourceBoneName, const FName TargetBoneName, const FName VirtualBoneRootName);
+};
+
 class USkeletonImporter : public IImporter {
 public:
 	USkeletonImporter(const FString& FileName, const FString& FilePath, const TSharedPtr<FJsonObject>& JsonObject, UPackage* Package, UPackage* OutermostPkg, TArray<TSharedPtr<FJsonValue>>& JsonObjects):
